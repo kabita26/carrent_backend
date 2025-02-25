eada9c5dd87a0a825f7372718a052c16b655a411
kabita26 <mahatok008@gmail.com>
2025-02-25 21:20:08 +0545

    reset controller

---
+++ controller/Resetpassport.js
@@ -0,0 +1,18 @@
+app.post('/reset-password/:email', (req, res) => {
+    const {email, token} = req.params
+    const {password} = req.body
+
+    jwt.verify(token, "jwt_secret_key", (err, decoded) => {
+        if(err) {
+            return res.json({Status: "Error with token"})
+        } else {
+            bcrypt.hash(password, 10)
+            .then(hash => {
+                UserModel.findByIdAndUpdate({_id: id}, {password: hash})
+                .then(u => res.send({Status: "Success"}))
+                .catch(err => res.send({Status: err}))
+            })
+            .catch(err => res.send({Status: err}))
+        }
+    })
+})
